62093f4d567fdce1947fb91b68da94236e7af5fb
Qambitions <63689257+Qambitions@users.noreply.github.com>
2021-06-15 22:12:26 +0700

    finish signup UI

---
+++ src/App/app/src/main/java/com/btree/beekey/SignUp.kt
@@ -0,0 +1,11 @@
+package com.btree.beekey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
