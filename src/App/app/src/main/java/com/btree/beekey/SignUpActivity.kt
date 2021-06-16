04c66fe491d776c9187cb7544baf71c770cbee4f
Qambitions <63689257+Qambitions@users.noreply.github.com>
2021-06-16 23:43:20 +0700

    fix name signupactivity.kt

--- src/App/app/src/main/java/com/btree/beekey/SignUpActivity.kt
+++ src/App/app/src/main/java/com/btree/beekey/SignUpActivity.kt
@@ -3,7 +3,7 @@ package com.btree.beekey
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignUp : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
