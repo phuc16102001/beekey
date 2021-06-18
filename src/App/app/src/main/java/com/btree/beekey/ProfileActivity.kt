122b7fd78002d64f64f212f7e681ae4978b2878b
thanhhoang4869 <thanhhoang4869@gmail.com>
2021-06-18 21:43:11 +0700

    stage

--- src/App/app/src/main/java/com/btree/beekey/ProfileActivity.kt
+++ src/App/app/src/main/java/com/btree/beekey/ProfileActivity.kt
@@ -2,10 +2,22 @@ package com.btree.beekey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivityActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.profile_activity)
+
+        val userList = mutableListOf<Feedback>()
+        userList.add(Feedback("Good","Description: Test test test","Thanh"))
+        userList.add(Feedback("Good","Description: Test test test","Phuc"))
+        userList.add(Feedback("Good","Description: Test test test","Khanh"))
+        userList.add(Feedback("Good","Description: Test test test","Khoa"))
+        Log.d("user",userList.size.toString());
+
+        val recycler_view: RecyclerView = findViewById(R.id.recycler)
+        recycler_view.adapter=FeedbackAdapter(userList)
     }
 }
