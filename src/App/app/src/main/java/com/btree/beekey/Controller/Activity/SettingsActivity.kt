4c6e3b4130804789498bbfdfde41d35690eba6a0
thanhhoang4869 <thanhhoang4869@gmail.com>
2021-07-02 19:52:57 +0700

    done setting backend

--- src/App/app/src/main/java/com/btree/beekey/Controller/Activity/SettingsActivity.kt
+++ src/App/app/src/main/java/com/btree/beekey/Controller/Activity/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.btree.beekey.Controller.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.btree.beekey.R
 
@@ -8,5 +11,27 @@ class SettingsActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val personalSetting: TextView = findViewById(R.id.personal_setting)
+        val changePassword: TextView = findViewById(R.id.password_setting)
+        val logoutButton: TextView = findViewById(R.id.logout_button)
+
+        personalSetting.setOnClickListener {
+            Intent(this, ChangeInformationActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        changePassword.setOnClickListener {
+            Intent(this, ChangePasswordActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        logoutButton.setOnClickListener{
+            this.cacheDir.deleteRecursively()
+            Intent(this,LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
