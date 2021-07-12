605a075ae612b53dab7007e40380a7f9d828fbd3
thanhhoang4869 <thanhhoang4869@gmail.com>
2021-07-12 23:27:08 +0700

    add select deadline To do: fix "null" displayed after choosing date

--- src/App/app/src/main/java/com/btree/beekey/Controller/Fragment/PostTaskFragment.kt
+++ src/App/app/src/main/java/com/btree/beekey/Controller/Fragment/PostTaskFragment.kt
@@ -1,15 +1,17 @@
 package com.btree.beekey.Controller.Fragment
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
+import com.btree.beekey.Controller.Activity.MainActivity
 import com.btree.beekey.Controller.Adapter.Category
 import com.btree.beekey.Model.CategoryResponse
 import com.btree.beekey.R
@@ -19,11 +21,15 @@ import com.btree.beekey.databinding.FragmentPostTaskBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
+import java.util.Calendar.MONTH
+import javax.xml.datatype.DatatypeConstants.MONTHS
+import kotlin.collections.ArrayList
 
 class PostTaskFragment : Fragment(R.layout.fragment_post_task) {
 
     private var _binding: FragmentPostTaskBinding? = null
-    private var categoryAdapter : ArrayAdapter<String>? = null
+    private var categoryAdapter: ArrayAdapter<String>? = null
     private var categoryList: List<Category>? = null
 
     // This property is only valid between onCreateView and
@@ -63,14 +69,17 @@ class PostTaskFragment : Fragment(R.layout.fragment_post_task) {
                 TODO("Not yet implemented")
             }
         }
+
+        selectDueDate()
+
     }
 
-    private fun loadAdapter(){
-        if (categoryList==null){
+    private fun loadAdapter() {
+        if (categoryList == null) {
             return
         }
 
-        val nameList : MutableList<String> = ArrayList()
+        val nameList: MutableList<String> = ArrayList()
         for (category in categoryList!!) {
             nameList.add(category.toString())
         }
@@ -79,8 +88,8 @@ class PostTaskFragment : Fragment(R.layout.fragment_post_task) {
                 it,
                 android.R.layout.simple_spinner_item,
                 nameList
-            ).also {
-                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 binding.spinnerCategory.adapter = adapter
             }
         }
@@ -106,10 +115,47 @@ class PostTaskFragment : Fragment(R.layout.fragment_post_task) {
             }
 
             override fun onFailure(call: Call<CategoryResponse>, t: Throwable) {
-                Toast.makeText(context,"Fail to connect to server",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Fail to connect to server", Toast.LENGTH_SHORT).show()
             }
         })
     }
+
+    private fun selectDueDate(){
+        val eText: EditText = binding.deadline;
+        eText.inputType = InputType.TYPE_NULL;
+        eText.setOnClickListener { eText.setText(selectDate()) }
+        }
+
+    private fun selectDate(): String {
+        val calendar = Calendar.getInstance()
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        var month = calendar.get(Calendar.MONTH)+1
+        val year = calendar.get(Calendar.YEAR)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        val second = calendar.get(Calendar.SECOND)
+        val string: EditText? =null
+        // date picker dialog
+        val datePicker =
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1,
+                    DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                        val monthI = month+1
+                        string?.setText(("$day/$monthI/$year").toString())
+                    },
+                    year,
+                    month,
+                    day,
+                )
+            }
+        datePicker?.show()
+        return string.toString()
+
+    }
 }
+
+
+
 
 
