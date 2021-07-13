28fb842c7d4876631b015a40db00ed96d02d6b21
phuc16102001 <phuc16102001@gmail.com>
2021-07-13 17:02:49 +0700

    wrong character when writing sql query

--- src/Server/models/counterOffer.js
+++ src/Server/models/counterOffer.js
@@ -3,7 +3,7 @@ const sql = require('./db')
 const CounterOffer = function(){};
 
 CounterOffer.postOffer = function(data,resultCallback) {
-    sql.query("INSERT INTO COUNTER-OFFER SET ?",
+    sql.query("INSERT INTO COUNTER_OFFER SET ?",
         data,
         function(err,res){
             if (err) {
@@ -17,7 +17,7 @@ CounterOffer.postOffer = function(data,resultCallback) {
 }
 
 CounterOffer.getByRequest = function(data,resultCallback) {
-    sql.query("SELECT task_id, lancer_id, reason, offer FROM COUNTER-OFFER WHERE task_id=?",
+    sql.query("SELECT task_id, lancer_id, reason, offer FROM COUNTER_OFFER WHERE task_id=?",
         data.task_id,
         function(err,res){
             if (err) {
