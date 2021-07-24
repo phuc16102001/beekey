fb3ce14107d0604fdbc6b500299deeb66ab1c7e2
KimKhanh14 <68914821+KimKhanh14@users.noreply.github.com>
2021-07-24 22:31:29 +0700

    Feedback-UI

--- src/App/app/src/main/java/com/btree/beekey/Controller/Activity/FeedbackActivity.kt
+++ src/App/app/src/main/java/com/btree/beekey/Controller/Activity/FeedbackActivity.kt
@@ -1,4 +1,12 @@
 package com.btree.beekey.Controller.Activity
 
-class FeedbackActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.btree.beekey.R
+
+class FeedbackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback)
+    }
 }
