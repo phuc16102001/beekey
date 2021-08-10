97134ff289b79c9c5a016194be87400643cf0697
Qambitions <63689257+Qambitions@users.noreply.github.com>
2021-08-10 19:52:57 +0700

    click task in my request

--- src/App/app/src/main/java/com/btree/beekey/Controller/Activity/MyListRequestActivity.kt
+++ src/App/app/src/main/java/com/btree/beekey/Controller/Activity/MyListRequestActivity.kt
@@ -1,7 +1,10 @@
 package com.btree.beekey.Controller.Activity
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.btree.beekey.Controller.Adapter.*
@@ -9,23 +12,49 @@ import com.btree.beekey.Model.ListTaskResponse
 import com.btree.beekey.R
 import com.btree.beekey.Utils.Cache
 import com.btree.beekey.Utils.MyAPI
+import com.btree.beekey.databinding.ActivityLoginBinding
+import com.btree.beekey.databinding.ActivityMyListRequestBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MyListRequestActivity:AppCompatActivity() {
-    private lateinit var listRequirement: List<Task>
+    private var listRequirement: List<Task>? = null
+    private lateinit var binding: ActivityMyListRequestBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMyListRequestBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_my_list_request)
 
         getListRequest(this)
     }
 
     private fun loadAdapter(){
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = TaskAdapter(listRequirement)
+        if (listRequirement == null) {
+            return
+        }
+        Log.d("getMyRequest", listRequirement.toString())
+        val taskAdapter = TaskAdapter(listRequirement!!)
+        taskAdapter.setClickListener(object : ItemClickListener {
+            override fun onClick(view: View, position: Int) {
+                Log.d("TAG","CLICK")
+                if (listRequirement!![position].status==Task.TASK_PENDING){
+                    val intent = Intent(this@MyListRequestActivity, RequestViewPendingActivity::class.java)
+                    intent.putExtra("task_id", listRequirement!![position].task_id)
+                    startActivity(intent)
+                }
+                if (listRequirement!![position].status==Task.TASK_DOING){
+                    val intent = Intent(this@MyListRequestActivity, RequestViewDoingActivity::class.java)
+                    intent.putExtra("task_id", listRequirement!![position].task_id)
+                    startActivity(intent)
+                }
+
+
+            }
+        })
+        val recyclerView = findViewById<RecyclerView>(R.id.listRequestRecyclerView)
+        recyclerView.adapter = taskAdapter
         recyclerView.setHasFixedSize(true)
     }
 
