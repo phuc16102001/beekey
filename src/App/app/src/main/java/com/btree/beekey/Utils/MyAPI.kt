f4b58be1565aa3cbbac57708cb3672edaa08d913
phuc16102001 <phuc16102001@gmail.com>
2021-08-21 02:14:45 +0700

    Change IP

--- src/App/app/src/main/java/com/btree/beekey/Utils/MyAPI.kt
+++ src/App/app/src/main/java/com/btree/beekey/Utils/MyAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MyAPI {
     companion object {
-        val BASE_URL = "http://171.232.49.2"
+        val BASE_URL = "http://115.79.226.47"
         val PORT = "8080"
 
         fun getAPI(): API {
